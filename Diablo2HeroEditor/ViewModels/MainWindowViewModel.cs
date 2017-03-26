2a02200ddb10d2ff33a8f1379501523740879783
Marc <blouin.marcandre@gmail.com>
2017-03-26 17:45:41 -0400

    Switch to MVVM

---
+++ Diablo2HeroEditor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Diablo2FileFormat;
+using Diablo2HeroEditor.Helpers;
+
+namespace Diablo2HeroEditor.ViewModels
+{
+    public class MainWindowViewModel : NotificationObject
+    {
+        public MainWindowViewModel()
+        {
+            Diablo2File file = new Diablo2File(@"C:\Users\Marc\Saved Games\Diablo II\Median.d2s");
+            file.Load();
+        }
+    }
+}
---
+++ Diablo2HeroEditor/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Diablo2HeroEditor.ViewModels;
+using System.Windows;
+
+namespace Diablo2HeroEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
